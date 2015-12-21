9abf4e1790c3bc6906e26afe8fb6520f8627af93
Michael Reeves <mike.reeves@gmail.com>
2015-12-21 00:34:19 -0800

    Little details

--- after.sh
+++ after.sh
@@ -9,3 +9,8 @@ composer install
 php artisan migrate:install
 php artisan migrate
 php artisan db:seed
+
+echo "Add the following lines to your /etc/hosts file so you can access the"
+echo "vagrant box laravel website from your localhost"
+echo "192.168.10.10   grandprix-run"
+echo "192.168.10.10   homestead.app"
